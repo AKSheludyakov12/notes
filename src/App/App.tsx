516dc63269c9ea6d78e22912c651061b77aecda6
AKSheludyakov12 <ak.sheludyakov@gmail.com>
2024-06-27 09:21:29 +1000

    Delete route

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -2,38 +2,15 @@
 import {  Route, Routes } from "react-router-dom";
 import { useEffect } from "react";
 import { MainPage } from "../pages/MainPage/ui/MainPage";
-import NewNote from "pages/NewNotePage/NewNote";
 import { getNote } from "./Providers/Redux/selectors/getNotes";
 import { useSelector } from "react-redux";
 
-// const handeEditNoteSubmit = (id, newTitle, newText) => {
-//   setNotes((prevNotes) =>{
-//     return prevNotes.map((note) =>{
-//       if (note.id === id){
-//         return {
-//           ...note,
-//           noteTitle: newTitle,
-//           noteText: newText
-//         }
-//       }
-//       return note
-//     })
-//   })
-// }
+
 const  App = () => {
 
 const notes = useSelector(getNote)
   return (
-    <div className="container">
-        <Routes>
-            <Route path="/" element={<MainPage  />} />
-            <Route
-              path="/NewNote"
-              element={<NewNote    />}
-            />
-          {/* <Route path="/Note/:id" element={<NotePage  notes={notes}  /> }></Route> */}
-        </Routes>
-    </div>
+       <MainPage/>
   );
 }
 
