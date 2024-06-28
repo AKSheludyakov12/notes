6ec90f22bcaa07e19d92620a82ee80e1b5ba1cb2
AKSheludyakov12 <ak.sheludyakov@gmail.com>
2024-06-28 19:24:48 +1000

    add sorted, add styles

--- src/widgets/header/ui/Header.tsx
+++ src/widgets/header/ui/Header.tsx
@@ -1,11 +1,14 @@
 import { MyInput } from "components/ui/input/MyInput"
 import { ClassNames } from "shared"
 import  cls from "./Header.module.scss"
+import { useState } from "react";
 
 
 interface HeaderProps {
     className?: string;
     searchQuery?:string;
+    sort: string;
+    setSort: (name:string) => void;
     onChangeInput?: (e) => void
 }
 
@@ -14,16 +17,30 @@ export const Header = (props:HeaderProps) => {
         className,
         searchQuery, 
         onChangeInput,
+        sort,
+        setSort,
         ...otherProps
      } = props
+
+     const filterList = [
+        {name: "ALL"},
+        {name: "Notes"},
+        {name: "Todos"}
+    ]
+
     return (
         <div className={ClassNames(cls.header, {}, [className])}>
             <MyInput
               value={searchQuery}
               placeholder="Search by the keyword..."
               onChange={onChangeInput}
+              className={cls.search_input}
             ></MyInput>
-            <h1 className={cls.title} >Notes</h1>
+            <div className={cls.filters}>
+                {filterList.map((filter, index)=> (
+                    <span className={filter.name === sort ? cls.active : cls.nonActive} onClick={()=>setSort(filter.name)}>{filter.name}</span>
+                ))} 
+            </div>
         </div>
     )
 } 
