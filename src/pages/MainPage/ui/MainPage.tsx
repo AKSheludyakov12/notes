92abf605b859e1a45dea56a6a3265e4005326e73
AKSheludyakov12 <ak.sheludyakov@gmail.com>
2024-06-28 19:21:14 +1000

    delete import, add states, add ContentList

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,59 +1,61 @@
 import { NoteDataAction } from "App/Providers/Redux/Slice/NoteDataSlice";
-import { StateSchema, noteData, noteSchema } from "App/Providers/Redux/config/StateScheme";
-import { ChangeEvent, HtmlHTMLAttributes, useCallback, useEffect, useMemo, useRef, useState } from "react";
+import { StateSchema, noteSchema } from "App/Providers/Redux/config/StateScheme";
+import { ChangeEvent, useCallback, useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import cls from "./MainPage.module.scss"
 import { Header } from "widgets/header";
-import { NoteList } from "./noteList/NoteList";
-import { AddNoteSvg, Modal } from "shared";
+import { AddNoteSvg } from "shared";
 import { Button, ButtonTheme } from "components/ui/button/MyButton";
 import { Popup } from "widgets/Popup";
+import { todoDataAction } from "App/Providers/Redux/Slice/TodoDataSlice";
+import { ContentList } from "./TodosAndNotesList/ContentList";
+import { NoteListEmpty } from "pages/NoteListEmpty/NoteListEmpty";
+
 
 
 
   export const MainPage = ( ) => {
       const dispatch = useDispatch()
-      const [serchValue, setSerchValue] = useState(" ")
-      const searchNote = useSelector((state:StateSchema)=>state.noteData.searchNote)
-      const noteData = useSelector((state:StateSchema)=>state.noteData.noteData)
+      const [searchValue, setSerchValue] = useState(" ")
       const [notes, setNotes] = useState([])
+      const [todos, setTodos] = useState([])
+      const [sort, setSort] = useState('ALL')
+      const searchNote = useSelector((state:StateSchema)=>state.noteData.searchNote)
+      const searchTodo = useSelector((state:StateSchema)=> state.todoData.searchTodo)
+      const noteData = useSelector((state:StateSchema)=>state.noteData.noteData)
+      const todoData = useSelector((state: StateSchema)=>state.todoData.todoData)
       const [isOpenPopup, setIsOpenPopup] = useState(false);
+    console.log(noteData)
 
-      const todoList = useSelector((state: StateSchema)=>state.todoData.todoData)
-      console.log(todoList)
-      
+      useEffect((
+        )=>{
+          setNotes(searchValue === " " ? noteData : searchNote)
+          setTodos(searchValue === " " ? todoData : searchTodo)
+        },[searchValue, searchNote, searchTodo, noteData, todoData, todoDataAction.addTodo, NoteDataAction.addNote, todoDataAction.addUpdatePoints,])
 
       const onChangeInput = (e: ChangeEvent<HTMLInputElement>) =>{
         dispatch(NoteDataAction.searchNote((e.target.value)))
+        dispatch(todoDataAction.searchTodo((e.target.value)))
         setSerchValue(e.target.value)
       }
       
+      
 
-      
-      const addNewNote = useCallback((newNote: noteSchema) => {
-        dispatch(NoteDataAction.addNote(newNote));
-        
-        
-      }, [dispatch]);
-      
-      useEffect(()=>{setNotes(serchValue === " " ? noteData :searchNote)},
-      [dispatch, addNewNote, noteData, searchNote,serchValue, NoteDataAction.deleteNote])
       return ( 
         
           <div className={cls.MainPage}>
-              <Header onChangeInput={onChangeInput}/>
-                <ul className={cls.notes}>
-                {notes.map((note, index) => (
-                  <NoteList
-                  note={note}
-                
-              />
-                ))}
-            
-              </ul>
+              <Header onChangeInput={onChangeInput} sort={sort}
+               setSort={setSort}/>
+              {noteData.length === 0 && todoData.length === 0 ? <NoteListEmpty/> : 
+              <ContentList
+              sort={sort}
+              notes={notes}
+              todos={todos}/>
+            }
+              
               {isOpenPopup && <Popup isOpen={isOpenPopup} setIsOpenPopup={setIsOpenPopup}
-              onAddNewNote={(newNote)=>addNewNote(newNote)} />}
-
+               />}
+              
               <Button theme={ButtonTheme.CIRCLE} 
               className={cls.pozition_button}
               onClick={()=>setIsOpenPopup(!isOpenPopup)}>
