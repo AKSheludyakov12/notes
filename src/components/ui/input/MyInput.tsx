f8e0eaadeb3ffc58b732759d5923c20a9428f832
AKSheludyakov12 <ak.sheludyakov@gmail.com>
2024-06-28 19:20:15 +1000

    removed unnecessary styles

--- src/components/ui/input/MyInput.tsx
+++ src/components/ui/input/MyInput.tsx
@@ -2,12 +2,14 @@ import React,{ ChangeEvent, InputHTMLAttributes, ReactNode } from 'react'
 import cls from './MyInput.module.scss'
 import { useDispatch } from 'react-redux'
 import { NoteDataAction } from 'App/Providers/Redux/Slice/NoteDataSlice'
+import { ClassNames } from 'shared'
 interface InputProps extends InputHTMLAttributes<HTMLElement>{
-    children?: ReactNode
+    children?: ReactNode,
+    className?: string
 }
-export const MyInput = ({children, ...props}: InputProps) =>{
+export const MyInput = ({children, className, ...props}: InputProps) =>{
     return ( 
-        <input {...props} className={cls.myInp}
+        <input {...props} className={ClassNames(cls.input, {}, [className])}
         >
             {children}
         </input>
