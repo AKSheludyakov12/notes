72a871bf4fe2e6e45bc572ce536209122a58e260
AKSheludyakov12 <ak.sheludyakov@gmail.com>
2024-06-28 19:25:34 +1000

    add styles, states, other

--- src/widgets/Popup/NewNote/ui/NewNote.tsx
+++ src/widgets/Popup/NewNote/ui/NewNote.tsx
@@ -1,38 +1,84 @@
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, Dispatch, SetStateAction, useEffect, useState } from "react";
 import { Modal } from "shared"
 import cls from "./NewNote.module.scss"
-import { useDispatch } from "react-redux";
-import randomColor from "randomcolor"
-import { on } from "events";
+import { useDispatch, useSelector } from "react-redux";
+import { NoteDataAction } from "App/Providers/Redux/Slice/NoteDataSlice";
+import { StateSchema } from "App/Providers/Redux/config/StateScheme";
 
-export const NewNote =  ({isOpen,onClose, onAddNewNote, setIsOpenPopup, setNewNoteModal}) => { 
+interface NoteProps {
+  isOpen:boolean,
+  onClose: ()=>void,
+  newNote: boolean,
+  noteText?: string,
+  noteTitle?: string,
+  noteId?:string; 
+  setNewNoteModal?: Dispatch<React.SetStateAction<boolean>>
+  setNoteText?: Dispatch<SetStateAction<string>>
+  setNoteTitle?: Dispatch<SetStateAction<string>>
+}
+
+export const NewNote =  (props:NoteProps) => {
   const dispatch = useDispatch()
-    const [noteText, setNoteText] = useState('');
-    const [noteTitle, setNoteTitle] = useState('');
-    const noteBackgroundColor = randomColor({
-      luminosity: 'light',
-   });
-    const handleNoteTextChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
-        setNoteText(event.target.value);
-      };
-      const handleNoteTitleChange = (event: ChangeEvent<HTMLInputElement>) => {
-        setNoteTitle(event.target.value);
-      };
-      const saveNote =() => {
-        const newNote = {noteTitle, noteText, noteBackgroundColor}
-        onAddNewNote(newNote)
-        setIsOpenPopup(false)
-        setNewNoteModal(false)
+
+  const {isOpen,
+    onClose,
+    setNewNoteModal,
+    newNote,
+    noteText,
+    setNoteText, 
+    setNoteTitle, 
+    noteId,
+    noteTitle
+  } = props 
+  const [newText, setNewText] = useState('');
+  const [newTitle, setNewTitle] = useState('');
+
+
+    const handlerCloseModal = () => { 
+      if(newTitle === "" && newText === ""){
+        dispatch(NoteDataAction.deleteNote(noteId))
       }
-    return( 
+      setNewNoteModal(false)
+    }
+
+
+
+      const handleUpdateNoteTextChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
+        const newNoteText = event.target.value;
+        setNewText(newNoteText);
+        setNoteText ? setNoteText(newNoteText) : setNewText(newNoteText)
+        setTimeout(() => dispatch(NoteDataAction.updateNoteText({id:noteId, text:newNoteText})));
+
+        };
+        const handleUpdateNoteTitleChange = (event: ChangeEvent<HTMLInputElement>) => {
+          const newNoteTitle = event.target.value;
+          setNoteTitle ? setNoteTitle(newNoteTitle) : setNewTitle(newNoteTitle)
+ 
+          setTimeout(() => dispatch(NoteDataAction.updateNoteTitle({id:noteId, text:newNoteTitle})), 400);
+        };
+
+if(newNote){
+  return( 
     <Modal
     isOpen={isOpen}
-    onClose={saveNote}>
-
-            <input type='text' placeholder='Заголовок' value={noteTitle} onChange={handleNoteTitleChange} className={cls.note__title}/>
+    onClose={handlerCloseModal}
+    >
+            <input type='text' placeholder='Заголовок' value={newTitle} onChange={handleUpdateNoteTitleChange} className={cls.note__title}/>
               <div className={cls.note__text}>
-              <textarea placeholder='Напишите что-нибудь...' value={noteText} onChange={handleNoteTextChange} className={cls.note__text}></textarea>
+              <textarea placeholder='Напишите что-нибудь...' value={newText} onChange={handleUpdateNoteTextChange} className={cls.note__text}></textarea>
             </div>
     </Modal>
     )
 }
+return (
+  <Modal
+  isOpen={isOpen}
+  onClose={onClose}
+  >
+          <input type='text' placeholder='Заголовок' value={noteTitle} onChange={handleUpdateNoteTitleChange} className={cls.note__title}/>
+            <div className={cls.note__text}>
+            <textarea placeholder='Напишите что-нибудь...' value={noteText} onChange={handleUpdateNoteTextChange} className={cls.note__text}></textarea>
+          </div>
+  </Modal>
+)
+  }
